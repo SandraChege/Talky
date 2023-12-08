666dd4e33d94691acd7c7945cb51bb95f9ddebbc
Sandra Chege <chegemwihaki88@gmail.com>
2023-12-08 10:40:50 +0300

    works on the profile page

--- Frontend/linkUp/src/app/summary/summary.component.ts
+++ Frontend/linkUp/src/app/summary/summary.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
-  styleUrls: ['./summary.component.css']
+  styleUrls: ['./summary.component.css'],
 })
 export class SummaryComponent {
+  isFormVisible: boolean = false;
 
+  viewFollowers() {
+    this.isFormVisible = true;
+  }
+  viewFollowing() {
+    this.isFormVisible = true;
+  }
+  hideform() {
+    this.isFormVisible = false;
+  }
 }
