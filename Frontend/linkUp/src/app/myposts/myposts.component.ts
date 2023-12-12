33368b5b81b37f14c223a9c5a6994d2e28f6a1f1
Sandra Chege <chegemwihaki88@gmail.com>
2023-12-12 17:20:19 +0300

    adds on the ui/ux of frontend

--- Frontend/linkUp/src/app/myposts/myposts.component.ts
+++ Frontend/linkUp/src/app/myposts/myposts.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-myposts',
   templateUrl: './myposts.component.html',
-  styleUrls: ['./myposts.component.css']
+  styleUrls: ['./myposts.component.css'],
 })
 export class MypostsComponent {
+  showReply: boolean = false;
 
+  toggleReply() {
+    this.showReply = true;
+  }
 }
